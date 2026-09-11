e8f54083d5f9a1e143425ae3c94e5d94548d32e1
arthurbento3 <arthurbento083@gmail.com>
2026-09-11 09:23:39 -0300

    salvando e alterando arquivos

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  if (menuToggle && navLinks) {
+    menuToggle.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+  }
+});
